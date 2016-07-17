24a13fb46c6c56643412a2f5916b92ff933153f2
ahdpeeb <ahdpeeb.h@gmail.com>
2016-07-17 12:37:33 +0300

    IOS project - init

---
+++ IOS Project/IOS Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS Project
+//
+//  Created by Nikola Andriiev on 17.07.16.
+//  Copyright © 2016 Andriiev.Mykola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
